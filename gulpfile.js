1946cbed351e0217f5faf365dea2479d1d3c7346
Evan <evan.levs@gmail.com>
2018-11-05 17:10:14 -0500

    update dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,56 @@
-const gulp = require('gulp');
-const connect = require('gulp-connect');
-const eslint = require('gulp-eslint');
-const babel = require('gulp-babel');
-var minify = require('gulp-minify');
+const gulp = require("gulp");
+const connect = require("gulp-connect");
+const eslint = require("gulp-eslint");
+const babel = require("gulp-babel");
+const minify = require("gulp-minify");
+const serveStatic = require("serve-static");
 
-gulp.task('lint', function () {
-  return gulp.src(['./src/*.js']).pipe(eslint()).pipe(eslint.format()).pipe(eslint.failAfterError());
+gulp.task("lint", function() {
+  return gulp
+    .src(["./src/*.js"])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
 });
 
-gulp.task('js', function () {
-  gulp.src('./src/*.js')
-    .pipe(connect.reload());
+gulp.task("js", function() {
+  gulp.src("./src/*.js").pipe(connect.reload());
 });
 
-gulp.task('connect', function () {
+gulp.task("connect", function() {
   connect.server({
-    root: './',
+    root: "./",
     livereload: true,
     port: 8888,
-    middleware: function (connect) {
-      return [connect().use('/bower_components', connect.static('bower_components'))];
+    middleware: function(connect) {
+      return [
+        connect().use("/bower_components", serveStatic("bower_components"))
+      ];
     }
   });
 });
 
-gulp.task('watch', function () {
-  gulp.watch(['./src/*.js'], ['lint', 'js']);
+gulp.task("watch", function() {
+  gulp.watch(["./src/*.js"], ["lint", "js"]);
 });
 
-gulp.task('build', function () {
-  return gulp.src('src/polygonize.js')
-    .pipe(babel({
-      presets: ['es2015']
-    }))
-    .pipe(minify({
-      ext: {
-        src:'.js',
-        min:'.min.js'
-      }
-    }))
-    .pipe(gulp.dest('dist'))
+gulp.task("build", function() {
+  return gulp
+    .src("src/polygonize.js")
+    .pipe(
+      babel({
+        presets: ["es2015"]
+      })
+    )
+    .pipe(
+      minify({
+        ext: {
+          src: ".js",
+          min: ".min.js"
+        }
+      })
+    )
+    .pipe(gulp.dest("dist"));
 });
 
-gulp.task('default', ['connect', 'lint', 'watch']);
+gulp.task("default", ["connect", "lint", "watch"]);
